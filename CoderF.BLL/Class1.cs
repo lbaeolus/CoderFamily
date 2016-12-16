4e31b1ac17328d59c533517361cd0b4fd6e52d03
JordanHmj <153017010@qq.com>
2016-12-16 10:46:36 +0800

    UnitTest

--- CoderF.BLL/Class1.cs
+++ CoderF.BLL/Class1.cs
@@ -8,5 +8,9 @@ namespace CoderF.BLL
 {
     public class Class1
     {
+        public int Square(int a,int b)
+        {
+            return a * b;
+        }
     }
 }
---
+++ CoderFUnitTest/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoderF.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderF.BLL.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void SquareTest()
+        {
+            Assert.IsTrue(new Class1().Square(2,8)==16);
+        }
+    }
+}
